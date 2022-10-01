9f941ac07d48ee1a4be57958435212b9a0d5d168
Gabriele Lopez <gabriele.ope@gmail.com>
2022-10-02 00:26:57 +0200

    Circle

--- Informatica/Cerchio/src/Main.java
+++ Informatica/Cerchio/src/Main.java
@@ -2,15 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double radius, area, circonference;
+        double radius, area, circumference;
 
         Scanner scanner = new Scanner(System.in);
 
         radius = scanner.nextFloat();
 
-        circonference = radius * 2 * Math.PI;
+        circumference = radius * 2 * Math.PI;
         area = Math.PI * Math.pow(radius, 2);
 
-        System.out.println("Circumference = " );
+        System.out.printf("%.2f%n", circumference);
+        System.out.printf("%.2f%n", area);
     }
 }

5783b9abcc1446b7d89ec3ae068bce75c000db3a
Gabriele Lopez <gabriele.ope@gmail.com>
2022-10-02 00:09:23 +0200

    JS beginner ex

---
+++ TPSIT/Beginners/static/js/functions.js
@@ -0,0 +1,56 @@
+function DaysInMonth(month, year) {
+    return new Date(year, month, 0).getDate();
+}
+
+function Vote(vote) {
+    if (vote < 6.0)
+        return "insufficient";
+    else if (vote > 8.0)
+        return "good";
+    else
+        return "adequate";
+}
+
+function GetMilliseconds(hours, minutes) {
+    return (hours * 60 * 60 * 1000) + (minutes * 60 * 1000)
+}
+
+function FahrenheitToDegrees(temperature) {
+    return (temperature -32.0) * 5.0 / 9.0;
+}
+
+function TimezoneHour(timezone, inputHour, inputMinute) {
+    timezone.toLowerCase();
+    let dt = luxon.DateTime.fromObject({hour: inputHour, minute: inputMinute}, {zone: luxon.DateTime.now().zoneName});
+    switch (timezone) {
+        case "new york":
+            return dt.setZone("America/New_York");
+        case "tokyo":
+            return dt.setZone("Asia/Tokyo");
+        case "moscow":
+            return dt.setZone("Europe/Moscow");
+    }
+    return -1;
+}
+
+/*
+let input, input2, input3;
+input = parseInt(prompt("Enter a month"));
+input2 = parseInt(prompt("Enter a year"));
+alert(DaysInMonth(input, input2) + " Days");
+
+input = parseFloat(prompt("Enter a vote"));
+alert(Vote(input));
+
+input = parseInt(prompt("Enter the hour"));
+input2 = parseInt(prompt("Enter the minute"));
+alert(GetMilliseconds(input, input2));
+
+input = parseFloat(prompt("Enter the temp"));
+alert(FahrenheitToDegrees(input));
+
+input = prompt("Enter a timezone - New York/Tokyo/Moscow");
+input2 = parseInt(prompt("Enter a hour"));
+input3 = parseInt(prompt("Enter a minute"));
+alert("The time in " + input + " is = " + TimezoneHour(input, input2, input3).hour + ":" + input3);
+*/
---
+++ TPSIT/Beginners/static/js/listeners.js
@@ -0,0 +1,30 @@
+document.getElementById("ex1Submit").addEventListener("click", function () {
+    let month = document.getElementById("ex1Month").value;
+    let year = document.getElementById("ex1Year").value;
+    document.getElementById("ex1Result").innerHTML = DaysInMonth(month, year).toString();
+})
+
+document.getElementById("ex2Submit").addEventListener("click", function () {
+    let vote = document.getElementById("ex2Vote").value;
+    document.getElementById("ex2Result").innerHTML = Vote(vote);
+})
+
+document.getElementById("ex3Submit").addEventListener("click", function () {
+    let hours = document.getElementById("ex3Hours").value;
+    let minutes = document.getElementById("ex3Minutes").value;
+    document.getElementById("ex3Result").innerHTML = GetMilliseconds(hours, minutes).toString();
+})
+
+document.getElementById("ex4Submit").addEventListener("click", function () {
+    let temp = document.getElementById("ex4Temp").value;
+    document.getElementById("ex4Result").innerHTML = FahrenheitToDegrees(temp).toString();
+})
+
+document.getElementById("ex5Submit").addEventListener("click", function () {
+    let timezone = document.getElementById("ex5Timezone").value.toLowerCase();
+    let hour = document.getElementById("ex5Hour").value;
+    let minute = document.getElementById("ex5Minute").value;
+    document.getElementById("ex5TimezoneO").innerHTML = timezone;
+    let output = TimezoneHour(timezone, hour, minute);
+    document.getElementById("ex5Result").innerHTML = output.hour +  ":" + output.minute;
+})

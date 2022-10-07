2bfbdd9ec912ac725075f245cfe85a761402fb17
Gabriele Lopez <gabriele.ope@gmail.com>
2022-10-07 21:20:10 +0200

    Fix

--- TPSIT/Beginners/static/js/functions.js
+++ TPSIT/Beginners/static/js/functions.js
@@ -5,7 +5,9 @@ function DaysInMonth(month, year) {
 function Vote(vote) {
     if (vote < 6.0)
         return "insufficient";
-    else if (vote > 8.0)
+    else if (vote <= 6.9)
+        return "sufficient";
+    else if (vote >= 7.9)
         return "good";
     else
         return "adequate";

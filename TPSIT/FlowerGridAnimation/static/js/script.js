0e9bdc9d02e1bd79deda24e3e1d1435185e72556
Gabriele Lopez <gabriele.ope@gmail.com>
2022-10-29 11:13:02 +0200

    Flower grid animation

---
+++ TPSIT/FlowerGridAnimation/static/js/script.js
@@ -0,0 +1,60 @@
+let w = 600;
+let h = 300;
+let diameter = 50;
+let radius = diameter / 2;
+
+function setup() {
+    createCanvas(w, h)
+    background("green");
+    /*
+     (let i = diameter; i < (w / diameter) * diameter; i += diameter * 2) {
+        for (let j = diameter; j < (h / diameter) * diameter; j += diameter * 2) {
+            drawFlower(i, j, diameter)
+        }
+    }
+    */
+}
+
+let i = diameter;
+let j = diameter;
+
+function draw() {
+    frameRate(5);
+    if (i < (w / diameter) * diameter && drawBool === true) {
+        drawFlower(i, j, diameter);
+        i += diameter * 2;
+    } else {
+        i = diameter;
+        j += diameter * 2;
+    }
+    if (j > (h / diameter) * diameter && drawBool === true) {
+        noLoop();
+    }
+}
+
+function drawFlower(x, y, d) {
+    strokeWeight(4);
+    stroke("black");
+    fill("orange");
+    circle(x - radius, y + radius, d)
+
+    strokeWeight(4);
+    stroke("black");
+    fill("orange");
+    circle(x + radius, y + radius, d)
+
+    strokeWeight(4);
+    stroke("black");
+    fill("orange");
+    circle(x - radius, y - radius, d)
+
+    strokeWeight(4);
+    stroke("black");
+    fill("orange");
+    circle(x + radius, y - radius, d)
+
+    strokeWeight(4);
+    stroke("black");
+    fill("red");
+    circle(x, y, diameter)
+}
